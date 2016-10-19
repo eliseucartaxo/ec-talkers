d39216a0d4a27934426090320afcc40d790e9710
Eliseu Cartaxo <eliseucartaxo@gmail.com>
2016-10-19 13:41:40 +0100

    Release test

---
+++ src/main/web-source-app/app/chat/room/room.service.js
@@ -0,0 +1,8 @@
+(function() {
+   'use strict';
+   angular.module('ec-talkers.room').factory('Room', Room);
+
+   function Room($resource) {
+      return ;
+   }
+})();
---
+++ src/main/web-source-app/app/chat/room/rooms.controller.js
@@ -0,0 +1,9 @@
+(function() {
+	'use strict';
+
+	angular.module('ec-talkers.room').controller('RoomsController', RoomsController);
+	
+	function RoomsController(Room){
+		var vm = this;
+	}
+})();
