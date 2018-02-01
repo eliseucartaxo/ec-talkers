4a6181b3a262e3c1cd0d2ba2bbe9638fe8d6ee1e
Eliseu Cartaxo <eliseucartaxo@users.noreply.github.com>
2018-02-01 11:26:36 +0000

    Update EcTalkersApplication.java

--- src/main/java/pt/ec/talkers/EcTalkersApplication.java
+++ src/main/java/pt/ec/talkers/EcTalkersApplication.java
@@ -13,5 +13,12 @@ public class EcTalkersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcTalkersApplication.class, args);		
+		if(1==1 && 2==2){
+			throw new NullPointerExeption();
+		}else if(2!=1) {
+			for(int i=2; i<2345;i++){
+				i -=1;
+			}
+		}
 	}
 }
